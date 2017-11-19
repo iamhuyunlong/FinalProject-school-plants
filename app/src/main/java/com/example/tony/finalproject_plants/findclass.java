d7212ac6b13eb9665e09b089e54d6646dec1f9e1
H2M1 <897510615@qq.com>
2017-11-19 21:00:43 +0800

    Search

--- app/src/main/java/com/example/tony/finalproject_plants/findclass.java
+++ app/src/main/java/com/example/tony/finalproject_plants/findclass.java
@@ -3,25 +3,38 @@ package com.example.tony.finalproject_plants;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * Created by user on 2017/10/23.
- */
 
 public class findclass extends Fragment {
     private Button finbtn;
+    private EditText inp;
+    private Plant respla;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.find, container, false);
         finbtn = view.findViewById(R.id.button);
+        inp = view.findViewById(R.id.searchtext);
         finbtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                String inf = inp.getText().toString();
                 Intent intent = new Intent(findclass.this.getActivity(),results.class);
-                startActivity(intent);
+                intent.putExtra("input",inf);
+                respla = Plant.getPlantByName(inf);
+                if (respla!=null) {
+                    startActivity(intent);
+                }
+                else{
+                    Toast toa = Toast.makeText(findclass.this.getActivity(),"No Result!",Toast.LENGTH_SHORT);
+                    toa.setGravity(Gravity.CENTER,10,200);
+                    toa.show();
+                }
             }
         });
         return view;
--- app/src/main/java/com/example/tony/finalproject_plants/results.java
+++ app/src/main/java/com/example/tony/finalproject_plants/results.java
@@ -1,12 +1,40 @@
 package com.example.tony.finalproject_plants;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class results extends Activity {
     @Override
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.results);
+        String inp = getIntent().getStringExtra("input");
+        Plant respla = Plant.getPlantByName(inp);
+        TextView plades = findViewById(R.id.results);
+        plades.setText(getDescription(respla.getName()));
+        ImageView plapic = findViewById(R.id.picture);
+        plapic.setImageBitmap(getPhoto(respla.getName()));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private String getDescription(String name){
+        Plant plant = Plant.getPlantByName(name);
+        FileUtils fu = new FileUtils(this.getApplicationContext());
+        String des = fu.getPlantDescription(plant);
+        return des;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private Bitmap getPhoto(String name){
+        Plant plant = Plant.getPlantByName(name);
+        FileUtils fu = new FileUtils(results.this.getApplicationContext());
+        Bitmap img = fu.getPlantBitmap(plant);
+        return img;
     }
 }
