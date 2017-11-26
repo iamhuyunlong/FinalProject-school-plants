8d32eda76d75509dd11a501ce60db2275b6cd1d2
Tony <850379795@qq.com>
2017-11-26 15:10:42 +0800

    update noteClass.refactor completely

--- app/src/main/java/com/example/tony/finalproject_plants/noteclass.java
+++ app/src/main/java/com/example/tony/finalproject_plants/noteclass.java
@@ -1,66 +1,101 @@
 package com.example.tony.finalproject_plants;
 
-import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.media.Image;
 import android.os.Build;
-import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
-import android.support.constraint.ConstraintLayout;
-import android.util.Log;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.graphics.Bitmap;
 
+import java.util.ArrayList;
 import java.util.List;
 
+public class noteClass extends Fragment {
 
-public class noteclass extends Fragment {
-    @Nullable
-    @Override
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.note, container, false);
-        List<Plant> plantList = Plant.getAllPlants();
-        int id[] = new int[5];
-        id[0] = R.id.photinia;
-        id[1] = R.id.planch;
-        id[2] = R.id.jacq;
-        id[3] = R.id.hance;
-        id[4] = R.id.linn;
-        int index = 0;
-        for (final Plant p:plantList)
-        {
-            p.imgbtn = view.findViewById(id[index]);
-            index += 1;  //Here is a big problem.
-            p.imgbtn.setImageBitmap(getPhoto(p.getName()));
-            p.imgbtn.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(noteclass.this.getActivity(),plants.class);
-                    intent.putExtra("name", p.getName());
-                    startActivity(intent);
-                }
-            });
-        }
-        return view;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private Bitmap getPhoto(String name){
+    @RequiresApi (api = Build.VERSION_CODES.KITKAT)
+    public Bitmap getPhoto(String name){
         Plant plant = Plant.getPlantByName(name);
-        FileUtils fu = new FileUtils(noteclass.this.getActivity().getApplicationContext());
+        FileUtils fu = new FileUtils(noteClass.this.getActivity().getApplicationContext());
         Bitmap img = fu.getPlantBitmap(plant);
         return img;
     }
 
-    public int dip2px(float dpValue){
-        final float scale = this.getResources().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
+    private class PlantAdapter extends ArrayAdapter<Plant> {
+        private int resourceId;
+
+        public PlantAdapter(Context context, int textViewResourceId, List<Plant> objects) {
+            super(context, textViewResourceId, objects);
+            resourceId = textViewResourceId;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Plant listPlants = getItem(position);
+            View view;
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+                viewHolder = new ViewHolder();
+                viewHolder.plantImage = (ImageView) view.findViewById(R.id.plant_image);
+                viewHolder.plantName = (TextView) view.findViewById(R.id.plant_name);
+                view.setTag(viewHolder);
+            } else {
+                view = convertView;
+                viewHolder = (ViewHolder) view.getTag();
+            }
+
+            viewHolder.plantImage.setImageBitmap(getPhoto(listPlants.getName()));
+            viewHolder.plantName.setText(listPlants.getName());
+            return view;
+        }
+
+        class ViewHolder{
+            ImageView plantImage;
+            TextView plantName;
+        }
+    }
+
+    private List<Plant> listViewPlants = new ArrayList<>();
+    private List<Plant> plantsList = Plant.getAllPlants();
+
+    @Nullable
+    @Override
+    @RequiresApi (api = Build.VERSION_CODES.KITKAT)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.note, container, false);
+        initlist_view_plants();
+
+        PlantAdapter adapter = new PlantAdapter(noteClass.this.getActivity(),R.layout.plants_item,listViewPlants);
+        ListView listView = view.findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,int position,long id){
+                Plant plant = listViewPlants.get(position);
+                Intent intent = new Intent(noteClass.this.getActivity(),plants.class);
+                intent.putExtra("name", listViewPlants.get(position).getName());
+                startActivity(intent);
+            }
+        });
+        return view;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void initlist_view_plants(){
+        for(Plant p:plantsList){
+            listViewPlants.add(p);
+        }
     }
 }
